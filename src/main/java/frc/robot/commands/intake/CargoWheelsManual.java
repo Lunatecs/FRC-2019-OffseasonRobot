9bdcdeba8c37c538cc9959374217adb57d049103
baxtrax <bradofficialbox@gmail.com>
2019-03-08 23:41:34 -0500

    Made a CargoWheelsManual command

---
+++ src/main/java/frc/robot/commands/intake/CargoWheelsManual.java
@@ -0,0 +1,71 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
+
+public class CargoWheelsManual extends Command {
+
+  private boolean applyConstantIntake = false;
+  public boolean debug = false;
+
+  public CargoWheelsManual() {
+    // Use requires() here to declare subsystem dependencies
+    requires(Robot.cargoIntake);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    double intakeSpeed = Robot.oi.getIntakeCargoSpeed();
+    double launchSpeed = Robot.oi.getIntakeCargoSpeed();
+
+    //TODO check if wheels are spinning right direction 
+    if(intakeSpeed > 0.2) {
+      Robot.cargoIntake.setCargoWheelSpeed(-intakeSpeed);
+      this.applyConstantIntake = true;
+    } else if(launchSpeed > 0.2) {
+      Robot.cargoIntake.setCargoWheelSpeed(launchSpeed);
+    } else {
+      if(applyConstantIntake) {
+        Robot.cargoIntake.setCargoWheelSpeed(-0.25);
+      } else {
+        Robot.cargoIntake.setCargoWheelSpeed(0);
+      }
+    }
+
+    if (debug) {
+      SmartDashboard.putNumber("Intake", intakeSpeed);
+      SmartDashboard.putNumber("Launch", launchSpeed);
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
