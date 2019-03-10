a7c35d63ed4a43c4bfacb5d5646bce7e8dac51d2
baxtrax <bradofficialbox@gmail.com>
2019-03-09 20:00:35 -0500

    Added LifeClimber command

--- src/main/java/frc/robot/commands/climber/LiftClimber.java
+++ src/main/java/frc/robot/commands/climber/LiftClimber.java
@@ -30,7 +30,7 @@ public class LiftClimber extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-      //------------------Climber------------------
+    //------------------Climber------------------
     
   }
 
