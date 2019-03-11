89ba68abc1f22af233c62372f0989c15a9567a04
TheDyingSun <thedyingsun.ng@gmail.com>
2019-03-11 19:21:19 -0400

    WHY OH WHY, FINISHED THREE HAB CLIMB

--- src/main/java/frc/robot/commands/autos/HabThreeClimb.java
+++ src/main/java/frc/robot/commands/autos/HabThreeClimb.java
@@ -19,31 +19,18 @@ public class HabThreeClimb extends CommandGroup {
    * Add your docs here.
    */
   public HabThreeClimb() {
-    // Add Commands here:
-    // e.g. addSequential(new Command1());
-    // addSequential(new Command2());
-    // these will run in order.
 
-    // To run multiple commands at the same time,
-    // use addParallel()
-    // e.g. addParallel(new Command1());
-    // addSequential(new Command2());
-    // Command1 and Command2 will run in parallel.
-
-    addSequential(new DropArms());
+    //addSequential(new DropArms());
     addSequential(new LiftRobotAuto(0));
     addSequential(new DriveClimberWheels());
     addSequential(new LiftClimber(0));
     addSequential(new AutoDrivetrain(0));
 
     //Lift Robot auto combines elevator and climber wheels
-    //TODO add a speed to the LRA LC
+    //TODO ADD VALUES TO GROUP
 
     
     // A command group will require all of the subsystems that each member
     // would require.
-    // e.g. if Command1 requires chassis, and Command2 requires arm,
-    // a CommandGroup containing them would require both the chassis and the
-    // arm.
   }
 }
